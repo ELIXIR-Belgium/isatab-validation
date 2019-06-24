a093d7ef3e5af49570fefaaa71490c66a39f7695
bedroesb <bedro@psb.vib-ugent.be>
2019-06-24 10:50:04 +0200

    update

---
+++ ISA-alidator_v1.py
@@ -0,0 +1,40 @@
+from sys import argv
+from isatools import isatab
+import os
+import json
+import codecs
+
+# Parameters
+# ----------
+
+isa_config_dir = "./isaconfig-phenotyping-basic"
+isa_tab_dir = argv[1]
+
+# Converting all characters to UTF-8
+# ----------------------------------
+
+files = [f for f in os.listdir(isa_tab_dir) if f.endswith('.txt')]
+
+for f in files:
+    with codecs.open(isa_tab_dir + os.sep + f, 'r') as file:
+        lines = file.read()
+    with codecs.open(isa_tab_dir + os.sep + f, 'w', encoding = 'utf8') as file:
+        print('Converting ' + str(isa_tab_dir + os.sep + f + ' to UTF-8.'))
+        file.write(lines)
+
+# Validating ISA-TAB with configuration files
+# -------------------------------------------
+
+try:
+    print('Validating isa-tab files against configuration files found in ' + isa_config_dir)
+    validation_log_path = isa_tab_dir + 'validation_log.json'
+    report = isatab.validate(open(os.path.join(isa_tab_dir, 'i_investigation.txt')), isa_config_dir)
+    with open(validation_log_path, 'w') as out_fp2:
+        json.dump(report, out_fp2, indent=4)
+    
+    print('VALIDATION FINISHED')
+    print('The ISA-TAB validation log file can be found at: ' + validation_log_path)
+
+except Exception as ioe:
+    print('ISA-TAB validation failed!...')
+    print(str(ioe))
