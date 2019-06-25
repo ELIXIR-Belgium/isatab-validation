d052123499581af83e4edd09bf92bdd3bb04416e
bedro <bedro@psb.ugent.be>
2019-06-25 16:17:06 +0200

    add temp file system

--- ISAtab-validator_v1.py
+++ ISAtab-validator_v1.py
@@ -3,6 +3,7 @@ from isatools import isatab
 import os
 import json
 import codecs
+import tempfile
 
 # Parameters
 # ----------
@@ -15,26 +16,28 @@ isa_tab_dir = argv[1]
 
 files = [f for f in os.listdir(isa_tab_dir) if f.endswith('.txt')]
 
-for f in files:
-    with codecs.open(isa_tab_dir + os.sep + f, 'r') as file:
-        lines = file.read()
-    with codecs.open(isa_tab_dir + os.sep + f, 'w', encoding = 'utf8') as file:
-        print('Converting ' + str(isa_tab_dir + os.sep + f + ' to UTF-8.'))
-        file.write(lines)
+with tempfile.TemporaryDirectory() as tmpdirname:  
+    print('Created temporary directory:', tmpdirname)
+    for f in files:
+        with codecs.open(isa_tab_dir + os.sep + f, 'r') as file:
+            lines = file.read()
+        with codecs.open(tmpdirname + os.sep + f, 'w', encoding = 'utf8') as file:
+            print('Converting ' + str(isa_tab_dir + os.sep + f + ' to UTF-8.'))
+            file.write(lines)
 
-# Validating ISA-TAB with configuration files
-# -------------------------------------------
+    # Validating ISA-TAB with configuration files
+    # -------------------------------------------
 
-try:
-    print('Validating isa-tab files against configuration files found in ' + isa_config_dir)
-    validation_log_path = isa_tab_dir + 'validation_log.json'
-    report = isatab.validate(open(os.path.join(isa_tab_dir, 'i_investigation.txt')), isa_config_dir)
-    with open(validation_log_path, 'w') as out_fp2:
-        json.dump(report, out_fp2, indent=4)
-    
-    print('VALIDATION FINISHED')
-    print('The ISA-TAB validation log file can be found at: ' + validation_log_path)
+    try:
+        print('Validating isa-tab files against configuration files found in ' + isa_config_dir)
+        validation_log_path = isa_tab_dir + os.sep + 'validation_log.json'
+        report = isatab.validate(open(os.path.join(tmpdirname, 'i_investigation.txt')), isa_config_dir)
+        with open(validation_log_path, 'w') as out_fp2:
+            json.dump(report, out_fp2, indent=4)
+        
+        print('VALIDATION FINISHED')
+        print('The ISA-TAB validation log file can be found at: ' + validation_log_path)
 
-except Exception as ioe:
-    print('ISA-TAB validation failed!...')
-    print(str(ioe))
+    except Exception as ioe:
+        print('ISA-TAB validation failed!...')
+        print(str(ioe))
